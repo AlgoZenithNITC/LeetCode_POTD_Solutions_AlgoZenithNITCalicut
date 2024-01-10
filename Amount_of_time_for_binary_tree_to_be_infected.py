96807b531a7234a365095bc6d6520161ea3db38e
AlgoZenithNITC <147307721+AlgoZenithNITC@users.noreply.github.com>
2024-01-10 21:54:19 +0530

    Create Amount_of_time_for_binary_tree_to_be_infected.py

---
+++ Amount_of_time_for_binary_tree_to_be_infected.py
@@ -0,0 +1,28 @@
+class Solution:
+    def __init__(self):
+        self.maxDistance = 0
+    
+    def amountOfTime(self, root, start):
+        self.traverse(root, start)
+        return self.maxDistance
+    
+    def traverse(self, root, start):
+        depth = 0
+        if root is None:
+            return depth
+        
+        leftDepth = self.traverse(root.left, start)
+        rightDepth = self.traverse(root.right, start)
+
+        if root.val == start:
+            self.maxDistance = max(leftDepth, rightDepth)
+            depth = -1
+        elif leftDepth >= 0 and rightDepth >= 0:
+            depth = max(leftDepth, rightDepth) + 1
+        else:
+            distance = abs(leftDepth) + abs(rightDepth)
+            self.maxDistance = max(self.maxDistance, distance)
+            depth = min(leftDepth, rightDepth) - 1
+        
+        return depth
+
