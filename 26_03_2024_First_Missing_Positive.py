c6966615d109d32986e5b74c5a250d2945ffd9e6
AlgoZenithNITC <147307721+AlgoZenithNITC@users.noreply.github.com>
2024-03-26 17:57:36 +0530

    Create 26_03_2024_First_Missing_Positive.py

---
+++ 26_03_2024_First_Missing_Positive.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def firstMissingPositive(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        n=len(nums)
+        seen=[False]*(n+1)
+
+        for num in nums:
+            if 0<num<=n:
+                seen[num]=True
+        for i in range(1,n+1):
+            if not seen[i]:
+                return i
+        return n+1
