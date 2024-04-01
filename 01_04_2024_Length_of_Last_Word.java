d738e994928d048c2396ce844e1685813a887a19
AlgoZenithNITC <147307721+AlgoZenithNITC@users.noreply.github.com>
2024-04-01 11:34:09 +0530

    Create 01_04_2024_Length_of_Last_Word.java

---
+++ 01_04_2024_Length_of_Last_Word.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Solution {
+    public int lengthOfLastWord(String s) {
+        int count = 0;
+        for(int i=s.length()-1 ; i>=0 ; i--) {
+            if(count==0 && s.charAt(i)==' ') {
+                continue;
+            }
+            else if(count!=0 && s.charAt(i)==' ') {
+                return count;
+            }
+            else if(s.charAt(i)!=' ') {
+                count++;
+            }
+        }
+        return count;
+    }
+}
+
