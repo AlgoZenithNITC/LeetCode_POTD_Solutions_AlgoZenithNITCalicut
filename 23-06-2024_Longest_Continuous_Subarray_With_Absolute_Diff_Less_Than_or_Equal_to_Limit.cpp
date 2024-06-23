4c44a698609d814d9597afa8003595bdcb9aa54b
AlgoZenithNITC <147307721+AlgoZenithNITC@users.noreply.github.com>
2024-06-23 16:34:28 +0530

    Create 23-06-2024_Longest_Continuous_Subarray_With_Absolute_Diff_Less_Than_or_Equal_to_Limit.cpp

---
+++ 23-06-2024_Longest_Continuous_Subarray_With_Absolute_Diff_Less_Than_or_Equal_to_Limit.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int longestSubarray(vector<int>& nums, int limit) {
+        ios_base::sync_with_stdio(0);
+        cin.tie(0); cout.tie(0);
+        
+        deque<int>increase;
+        deque<int>decrease;
+        int max_len = 0;
+        int left = 0;
+
+        for(int right = 0; right < nums.size(); right++){
+            while(!increase.empty() && nums[right] < increase.back()){
+                increase.pop_back();
+            }
+            increase.push_back(nums[right]);
+
+            while(!decrease.empty() && nums[right] > decrease.back()){
+                decrease.pop_back();
+            }
+            decrease.push_back(nums[right]);
+
+            while(decrease.front() - increase.front() > limit){
+                if(nums[left] == decrease.front()){
+                    decrease.pop_front();
+                }
+                if(nums[left] == increase.front()){
+                    increase.pop_front();
+                }
+                left++;
+            }
+            max_len = max(max_len, right-left+1);
+        }
+        return max_len;
+    }
+};
