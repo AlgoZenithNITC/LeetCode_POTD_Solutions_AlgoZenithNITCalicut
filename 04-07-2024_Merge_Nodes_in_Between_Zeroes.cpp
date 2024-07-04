909f8b7cd9a70e1dc09682ff4f7653d6cd0f4798
AlgoZenithNITC <147307721+AlgoZenithNITC@users.noreply.github.com>
2024-07-04 16:35:30 +0530

    Create 04-07-2024_Merge_Nodes_in_Between_Zeroes.cpp

---
+++ 04-07-2024_Merge_Nodes_in_Between_Zeroes.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    ListNode* mergeNodes(ListNode* head) {
+        ListNode* current = head->next; // Skip the initial zero node
+        ListNode* newHead = current; // This will be the new head of the merged list
+        ListNode* prev = nullptr;
+        int sum = 0;
+
+        while (current) {
+            if (current->val == 0) { // When encountering a zero
+                current->val = sum; // Set the current node's value to the sum
+                if (prev) {
+                    prev->next = current; // Connect the previous non-zero node to the current node
+                }
+                else
+                  newHead=current;
+                prev = current; // Update the previous node
+                sum = 0; // Reset the sum
+            } else {
+                sum += current->val; // Add the current node's value to the sum
+            }
+            current = current->next; // Move to the next node
+        }
+
+        return newHead; // Return the new head of the modified list
+    }
+};
