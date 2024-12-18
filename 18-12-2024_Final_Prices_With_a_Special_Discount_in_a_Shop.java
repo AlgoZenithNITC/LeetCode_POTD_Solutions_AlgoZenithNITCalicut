6a05d6ef4b8b7718d862497364bf7ec918db966c
AlgoZenithNITC <147307721+AlgoZenithNITC@users.noreply.github.com>
2024-12-18 18:03:26 +0530

    Create 18-12-2024_Final_Prices_With_a_Special_Discount_in_a_Shop.java

---
+++ 18-12-2024_Final_Prices_With_a_Special_Discount_in_a_Shop.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[] finalPrices(int[] prices) {
+        int[] ans = prices.clone();
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < prices.length; i++) {
+            while (!stack.isEmpty() && prices[stack.peek()] >= prices[i]) {
+                ans[stack.pop()] -= prices[i];
+            }
+            stack.add(i);
+        }
+        return ans;
+    }
+}
