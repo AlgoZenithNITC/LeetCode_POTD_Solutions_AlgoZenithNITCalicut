1090e626f320f85dc80e0d74af572e0f20b0b2dd
AlgoZenithNITC <147307721+AlgoZenithNITC@users.noreply.github.com>
2024-12-31 21:25:08 +0530

    Create 31-12-2024_Minimum_Cost_For_Tickets.py

---
+++ 31-12-2024_Minimum_Cost_For_Tickets.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def mincostTickets(self, days, costs):
+        dp={len(days): 0}
+        def dfs(i):
+            if i in dp:
+                return dp[i]
+            dp[i] = float("inf")
+            j=i
+            for cost, duration in zip(costs, [1, 7, 30]):
+                while j<len(days) and days[j]<days[i]+duration:
+                    j+=1
+                dp[i] = min(dp[i], cost+dfs(j))
+            return dp[i]
+        return dfs(0)
