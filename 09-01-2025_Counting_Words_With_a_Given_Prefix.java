ab6812cd1dd640ccdf8fe4782a8dbea57a72e255
AlgoZenithNITC <147307721+AlgoZenithNITC@users.noreply.github.com>
2025-01-09 19:51:58 +0530

    Create 09-01-2025_Counting_Words_With_a_Given_Prefix.java

---
+++ 09-01-2025_Counting_Words_With_a_Given_Prefix.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int prefixCount(String[] words, String pref) {
+        int count=0;
+        int len= pref.length();
+        for(String word:words){
+            if(len<=word.length()){
+                String sub = word.substring(0, len);
+                if(pref.equals(sub)){
+                    count++;
+                }
+            }
+        }
+        return count;
+        
+    }
+}
