62b39b33859fa66d1e6b6868b60edd569caf6b94
AlgoZenithNITC <147307721+AlgoZenithNITC@users.noreply.github.com>
2025-01-12 18:08:44 +0530

    12-01-2025_Check_if_a_Parentheses_String_Can_Be_Valid.py

---
+++ 12-01-2025_Check_if_a_Parentheses_String_Can_Be_Valid.py
@@ -0,0 +1,31 @@
+def canBeValid(s: str, locked: str) -> bool:
+    n = len(s)
+    if n % 2 != 0:
+        return False
+
+    open_count = 0
+    close_count = 0
+    unlocked1 = 0
+    unlocked2 = 0
+
+    for i in range(n):
+        if locked[i] == '0':
+            unlocked1 += 1
+        elif s[i] == '(':
+            open_count += 1
+        elif s[i] == ')':
+            open_count -= 1
+        if open_count + unlocked1 < 0:
+            return False
+
+        j = n - i - 1
+        if locked[j] == '0':
+            unlocked2 += 1
+        elif s[j] == ')':
+            close_count += 1
+        elif s[j] == '(':
+            close_count -= 1
+        if close_count + unlocked2 < 0:
+            return False
+
+    return True
