a1011b71d4a627bdf7798233a0d64c06deb5d25d
AlgoZenithNITC <147307721+AlgoZenithNITC@users.noreply.github.com>
2025-06-13 10:15:33 +0530

    Add files via upload
    06JUNE2025_leetcode_potd

---
+++ 06_06_2025_Using_a_Robot_to_Print_the_Lexicographically_Smallest_String.cpp
@@ -0,0 +1,25 @@
+int n = s.size();
+    vector<char> minSuffix(n);
+    minSuffix[n - 1] = s[n - 1];
+
+    for (int i = n - 2; i >= 0; --i)
+        minSuffix[i] = min(s[i], minSuffix[i + 1]);
+
+    stack<char> t;
+    string result;
+
+    for (int i = 0; i < n; ++i) {
+        t.push(s[i]);
+        while (!t.empty() && (i == n - 1 || t.top() <= minSuffix[i + 1])) {
+            result += t.top();
+            t.pop();
+        }
+    }
+
+    while (!t.empty()) {
+        result += t.top();
+        t.pop();
+    }
+
+    return result;
+
