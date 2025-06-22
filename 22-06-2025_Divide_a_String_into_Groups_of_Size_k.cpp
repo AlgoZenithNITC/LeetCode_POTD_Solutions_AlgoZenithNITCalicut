e1d74a1ea1e9b2bc56b36823246ca038e388961e
AlgoZenithNITC <147307721+AlgoZenithNITC@users.noreply.github.com>
2025-06-22 17:18:13 +0530

    Create 22-06-2025_Divide_a_String_into_Groups_of_Size_k.cpp

---
+++ 22-06-2025_Divide_a_String_into_Groups_of_Size_k.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<string> divideString(string s, int k, char fill) {
+       vector<string> result;
+        int n = s.length();
+        for(int i=0; i<n; i=i+k){
+            string temp = s.substr(i, k);
+            if(temp.length()<k){
+                temp+=string(k-temp.length(), fill);
+            }
+            result.push_back(temp);
+        }
+        return result;
+        
+        
+    }
+};
