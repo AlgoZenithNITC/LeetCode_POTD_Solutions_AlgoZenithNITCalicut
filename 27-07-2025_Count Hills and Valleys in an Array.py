53d0b3c3b54b6449b8a334781b8ee0cc045ed531
AlgoZenithNITC <147307721+AlgoZenithNITC@users.noreply.github.com>
2025-07-27 22:15:22 +0530

    Add files via upload

---
+++ 27-07-2025_Count Hills and Valleys in an Array.py
@@ -0,0 +1,15 @@
+class Solution:
+    def countHillValley(self, nums: List[int]) -> int:
+        n = len(nums)
+        i = 0
+        j = 1
+        count = 0
+        
+        while j + 1 < n:
+            if (nums[i] < nums[j] and nums[j] > nums[j + 1]) or \
+               (nums[i] > nums[j] and nums[j] < nums[j + 1]):
+                count += 1
+                i = j
+            j += 1
+        
+        return count
