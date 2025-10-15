6c7e70a1d513e8a4ab49919d621f4c82ba584dae
AlgoZenithNITC <147307721+AlgoZenithNITC@users.noreply.github.com>
2025-10-15 13:24:58 +0530

    Create 15-10-2025_Adjacent_Increasing_Subarrays_Detection_II.py

---
+++ 15-10-2025_Adjacent_Increasing_Subarrays_Detection_II.py
@@ -0,0 +1,25 @@
+class Solution:
+    def maxIncreasingSubarrays(self, nums):
+        n = len(nums)
+        cnt = 1
+        prev = 0
+        max_val = 0
+
+        for i in range(1, n):
+            if nums[i] > nums[i - 1]:
+                cnt += 1
+            else:
+                prev = cnt
+                cnt = 1
+
+            temp = prev
+            if cnt < prev:
+                temp = cnt
+
+            if temp > max_val:
+                max_val = temp
+
+            if cnt // 2 > max_val:
+                max_val = cnt // 2
+
+        return max_val
